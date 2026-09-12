2d29110e192bcdf93511836a85e065615bec93f1
orbiq379-art <orbiq379@gmail.com>
2026-09-12 14:03:50 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
@@ -21,20 +22,16 @@ dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
 
     implementation("androidx.activity:activity-ktx:1.10.0")
-
     implementation("androidx.activity:activity-compose:1.10.0")
 
     implementation(platform("androidx.compose:compose-bom:2024.12.01"))
 
     implementation("androidx.compose.ui:ui")
-
     implementation("androidx.compose.ui:ui-graphics")
-
     implementation("androidx.compose.ui:ui-tooling-preview")
 
     implementation("androidx.compose.material3:material3")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
-
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
